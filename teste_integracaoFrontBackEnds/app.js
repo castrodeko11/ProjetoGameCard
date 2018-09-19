29b2eaa2210bc559039f0ba8d7ef1a18366e9646
lucasd19971113 <lucas_d_motta@outlook.com>
2018-09-19 02:25:46 -0300

    Bug fix nas portas do servidor. Porta setada para Windows e porta para MacOSX

--- teste_integracaoFrontBackEnds/app.js
+++ teste_integracaoFrontBackEnds/app.js
@@ -1,7 +1,11 @@
 /* importar as configurações do servidor */
 var app = require('./config/server');
 
+var PORT = process.env.PORT || 3000; /* para MacOSX
+
+//var PORT = process.env.PORT || 80;  /* para Windows 
+
 /* parametrizar a porta de escuta */
-app.listen(80, function(){
+app.listen(PORT, function(){
 	console.log('Servidor online');
 })
