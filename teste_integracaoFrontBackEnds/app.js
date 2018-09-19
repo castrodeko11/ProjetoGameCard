ec77e5d6e5fbfaf710343b2270b205cfe5470ec8
Lucas Figueira <lucas.figueira@sempreit.com.br>
2018-09-19 20:07:07 -0300

    Portas setadas

--- teste_integracaoFrontBackEnds/app.js
+++ teste_integracaoFrontBackEnds/app.js
@@ -3,7 +3,7 @@ var app = require('./config/server');
 
 var PORT = process.env.PORT || 3000; /* para MacOSX
 
-//var PORT = process.env.PORT || 80;  /* para Windows 
+var PORT = process.env.PORT || 80;  /* para Windows 
 
 /* parametrizar a porta de escuta */
 app.listen(PORT, function(){
