a22acc2ccb2ac39978a4cd485c22f909596d175a
lucasd19971113 <lucas_d_motta@outlook.com>
2018-09-23 13:39:10 -0300

    package.json update

--- teste_integracaoFrontBackEnds/app.js
+++ teste_integracaoFrontBackEnds/app.js
@@ -1,7 +1,7 @@
 /* importar as configurações do servidor */
 var app = require('./config/server');
 
-var PORT = process.env.PORT || 3000; /* para MacOSX
+var PORT = process.env.PORT || 5000; /* para MacOSX
 
 //var PORT = process.env.PORT || 80;  /* para Windows 
 
