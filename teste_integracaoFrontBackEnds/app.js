7e00a502dfc6aaabb2abb422e9d2072d669481ba
André Oliveira <castrodeko11@hotmail.com>
2018-09-23 21:23:08 -0300

    ACO

--- teste_integracaoFrontBackEnds/app.js
+++ teste_integracaoFrontBackEnds/app.js
@@ -1,11 +1,8 @@
 /* importar as configurações do servidor */
 var app = require('./config/server');
 
-<<<<<<< HEAD
+
 //var PORT = process.env.PORT || 3000; /* para MacOSX
-=======
-var PORT = process.env.PORT || 5000; /* para MacOSX
->>>>>>> andreOliveira
 
 var PORT = process.env.PORT || 80;  /* para Windows 
 
