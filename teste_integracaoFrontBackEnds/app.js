593cd20b9b56c3b9264343d42daee60102b3c581
Lucas Figueira <lucas.figueira@sempreit.com.br>
2018-10-04 16:22:41 -0300

    Ajuste na env.PORT/env._Port

--- teste_integracaoFrontBackEnds/app.js
+++ teste_integracaoFrontBackEnds/app.js
@@ -3,15 +3,17 @@ var app = require('./config/server');
 require('dotenv').config();
 //var PORT = process.env.PORT || 5000; /* para MacOSX
 
-const port = process.env.PORT || 5000;  /* para Windows */
+const port = process.env.PORT || 5000;
+
+const _port = process.env._Port || 1080/* para Windows */
 
 	/* parametrizar a porta de escuta */
 	app.listen(port, () => {
 		console.log("Servidor online na porta padrão: "+port)
 	}).on('error', (err) => {
 		if((err.errno === 'EADDRINUSE') || (err.errno === 'EACCES')){
-			app.listen(port, () => {
-				console.log("Servidor online na porta: "+port);
+			app.listen(_port, () => {
+				console.log("Servidor online na porta: "+_port);
 		});
 	}
 });
